67e2187459fbe0c24b68f3de0a573c731fdbf6ae
Simon Hunter <33091112+Simon-Hunter@users.noreply.github.com>
2017-12-20 09:23:28 +0000

    Pipes added, filtering in class added

--- src/app/properties/properties-list.component.ts
+++ src/app/properties/properties-list.component.ts
@@ -1,20 +1,35 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {IProperty} from "./property";
+
 
 @Component ({
   selector: 'rent-properties',
-  templateUrl: './properties-list.component.html'
+  templateUrl: './properties-list.component.html',
+  styleUrls: ['./properties-list.component.css']
 })
-export class PropertiesListComponent {
+export class PropertiesListComponent implements OnInit {
+
   pageTitle: string = "Properties Listing";
   imageWidth: number = 100;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = '2 Bed';
-  properties: any [] = [
+  private _listFilter: number;
+  get listFilter(): number {
+    return this._listFilter;
+  }
+
+  set listFilter(value: number) {
+    this._listFilter = value;
+    this.filteredProperties = this.listFilter ? this.performFilter(this.listFilter) : this.properties;
+  }
+
+  filteredProperties: IProperty[];
+
+  properties: IProperty [] = [
     {
       "propertyId": 1,
       "propertyDesc": "1 Bed Flat, Viewforth Sqr, EH10",
-      "noBedrooms": "1",
+      "noBedrooms": 1,
       "availableDate": "January 19, 2018",
       "description": "Second floor fully modernized 2 bedroom flat set in the highly desirable area of Polwarth, with a host of great local ameni... ",
       "rentalPrice": 940,
@@ -25,7 +40,7 @@ export class PropertiesListComponent {
     {
       "propertyId": 2,
       "propertyDesc": "3 Bed Flat, Napier Rd, EH10",
-      "noBedrooms": "3",
+      "noBedrooms": 3,
       "availableDate": "March 12, 2018",
       "description": "Second floor fully modernized 3 bedroom flat set in the highly desirable area of Merchiston, with a host of great local ameni... ",
       "rentalPrice": 450,
@@ -35,7 +50,21 @@ export class PropertiesListComponent {
     },
   ];
 
+  constructor(){
+    this.filteredProperties = this.properties;
+    this.listFilter = 1;
+  }
+
+  performFilter(filterBy: number): IProperty[]{
+    return this.properties.filter((property: IProperty) =>
+        property.noBedrooms >= filterBy)
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
---
+++ src/app/properties/property.ts
@@ -0,0 +1,11 @@
+export interface IProperty {
+  propertyId: number;
+  propertyDesc: string;
+  noBedrooms: number;
+  availableDate: string;
+  description: string;
+  rentalPrice: number;
+  priceFreq: string;
+  starRating: number;
+  imageUrl: string;
+}
